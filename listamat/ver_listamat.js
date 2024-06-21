ba79ee022c8cd5793e7b6a2768220791ea93bfff
Carlos Martin <carlos.augusto.martin@gmail.com>
2024-06-21 15:58:06 -0300

    v14.3 upgrade listamat

--- listamat/ver_listamat.js
+++ listamat/ver_listamat.js
@@ -47,6 +47,7 @@ function myFunction() {
   
     // Loop through all table rows, and hide those who don't match the search query
     for (i = 0; i < tr.length; i++) {
+      if (filter.length>2){
         td0 = tr[i].getElementsByTagName("td")[0];
         td1 = tr[i].getElementsByTagName("td")[1];
         if (td0) {
@@ -58,7 +59,8 @@ function myFunction() {
             tr[i].style.display = "none";
           }
         }
-    }
+      }
+      }
   }
 
 function ver_stock(material) {
@@ -140,64 +142,6 @@ function ver_stock(material) {
     actualizado.innerHTML = "Información actualizada al: " + obtenerFecha(fecha)
     divshow.appendChild(actualizado)
 
-/*     let responsable = document.createElement("p")
-    responsable.setAttribute ("class", "texto_issue")
-    try {
-      responsable.innerHTML = "Responsable: " + issue.fields.assignee.displayName
-    } catch {
-      responsable.innerHTML = "Responsable: No asignado"
-    }
-    divshow.appendChild(responsable)
-
-    let informador = document.createElement("p")
-    informador.setAttribute ("class", "texto_issue")
-    informador.innerHTML = "Informador: " + issue.fields.reporter.displayName
-    divshow.appendChild(informador)
-
-    let fecha_creacion = document.createElement("p")
-    fecha_creacion.setAttribute ("class", "texto_issue")
-    fecha_creacion.innerHTML = "Creada: " + issue.fields.customfield_11282
-    divshow.appendChild(fecha_creacion)
-
-    let fecha_necesidad = document.createElement("p")
-    fecha_necesidad.setAttribute ("class", "texto_issue")
-    fecha_necesidad.innerHTML = "Fecha Vencimiento: " + issue.fields.duedate
-    divshow.appendChild(fecha_necesidad)
-
-    let fecha_planificada = document.createElement("p")
-    fecha_planificada.setAttribute ("class", "texto_issue")
-    fecha_planificada.innerHTML = "Fecha Planificada: " + issue.fields.customfield_12174
-    divshow.appendChild(fecha_planificada)
-
-    let edificio = document.createElement("p")
-    edificio.setAttribute ("class", "texto_issue")
-    try{
-      edificio.innerHTML = "Edificio: " + issue.fields.customfield_13399.value
-    } catch {
-      edificio.innerHTML = "Edificio: No informado"
-    }
-    divshow.appendChild(edificio)
-
-    let pep = document.createElement("p")
-    pep.setAttribute ("class", "texto_issue")
-    pep.innerHTML = "PEP: " + issue.fields.customfield_13414
-    divshow.appendChild(pep)
-
-    let padre = document.createElement("p")
-    padre.setAttribute ("class", "texto_issue")
-    try{
-      padre.innerHTML = "Tarea Padre: " + issue.fields.parent.key + " - " + issue.fields.parent.fields.summary
-    } catch {
-      padre.innerHTML = "Tarea Padre: No Tiene"
-    }
-    divshow.appendChild(padre)
-
-    let boton = document.createElement("button")
-    boton.setAttribute ("class", "btn btn-primary")
-    boton.setAttribute ("onclick",`openURL('${issue.key}')`)
-    boton.innerHTML = "Abrir en Jira"
-    divshow.appendChild(boton)
- */
 }
 
 let col2 = ["#","Material","Descripción","u.m.","Cantidad","USD Unit","Subtotal","Acción"];
@@ -219,15 +163,17 @@ const titulo2 = document.createElement("h5")
 titulo2.innerHTML = "Tareas Seleccionadas"
 
 const boton1 = document.createElement("button")
-boton1.innerHTML = "Generar OE"
-boton1.setAttribute("onclick", "generar_OE()");
+boton1.innerHTML = "Plantilla MM"
+boton1.setAttribute("onclick", "generar_MM()");
 boton1.setAttribute("class", "btn btn-success");
 
+/* 
 const boton2 = document.createElement("button")
 boton2.innerHTML = "Agregar viático"
 boton2.setAttribute("onclick", "agregar_viatico()");
 boton2.setAttribute("class", "btn btn-success");
 boton2.setAttribute("id", "boton2");
+ */
 
 const titulo6 = document.createElement("p")
 titulo6.setAttribute("id","subtotal_OE")
@@ -289,19 +235,162 @@ function obtenerFecha(fechaExcel) {
   return fechaJS.getDate() + "/" + (fechaJS.getMonth()+1) + "/" + fechaJS.getFullYear()
 }
 
-/*
-let col2 = ["#","Pos NPA","Tarea","Cantidad","Precio","Subtotal","Acción"];
+function calcular_subtotal(cantidad,id_row){
 
-const table2 = document.createElement("table");
-table2.setAttribute("id", "myTable2");
-table2.setAttribute("class", "table");
-table2.setAttribute("class", "table table-light table-striped");
-
-let tr2 = table2.insertRow(-1);                   // table row.
-
-for (let i = 0; i < col2.length; i++) {
-    let th2 = document.createElement("th");      // table header.
-    th2.innerHTML = col2[i];
-    tr2.appendChild(th2);
+  let tabla = document.getElementById("myTable2")
+  let subtotal=0
+  for (i=1; i<tabla.rows.length; i++){
+      if (tabla.rows[i].cells[0].textContent==id_row){
+          tabla.rows[i].cells[5].textContent= "$"+((Math.round(parseFloat(tabla.rows[i].cells[4].textContent.slice(1))*cantidad*100))/100).toString()
+      }
+      subtotal=subtotal+parseFloat(tabla.rows[i].cells[5].textContent.slice(1))
+      console.log(subtotal)
+  }
+//    titulo6.innerHTML= "Monto Total OE:  $"+subtotal       
 }
+
+function eliminar_tarea(fila){
+  let tabla= document.getElementById("myTable2")
+  for (let i=0; i<tabla.rows.length;i++){
+      if (tabla.rows[i].cells[0].textContent==fila){
+          tabla.deleteRow(i)
+      }
+  }
+}
+
+function generar_OE(){
+
+  let fecha = new Date();
+  fecha.setDate(fecha.getDate()+15)
+  let fecha_entrega = fecha.getDate()+"/"+(fecha.getMonth()+1)+"/"+fecha.getFullYear()
+
+  divShowData4.style.display="none"
+  divShowData.style.display="none"
+  divShowData2.style.display="none"
+  divShowData3.style.display="block"
+
+  let tabla= document.getElementById("myTable2")
+  let filas=[]
+  let fila= []
+  let posicion
+  let cantidad_oe
+  let descripcion
+  let precio
+  let precio_total
+  let total=0
+
+  for (i=1; i<tabla.rows.length; i++){
+       let posNPA=tabla.rows[i].cells[1].textContent
+       let cantidad=tabla.rows[i].cells[3].children.cantidad.value
+       if (parseInt(posNPA)==posNPA){
+          posicion= posNPA
+          cantidad_oe=cantidad
+          descripcion= tabla.rows[i].cells[2].textContent
+          precio= parseFloat(tabla.rows[i].cells[4].textContent.slice(1))
+          precio_total= (Math.round(parseFloat(cantidad)*precio*100))/100
+          fila=[npa_proveedor, "0010", posicion, descripcion, fecha_entrega, cantidad_oe, precio, precio_total]
+          filas.push(fila)
+          } else {
+              for (let j = 0; j < tareas_agrupadas.Agrupados.length; j++){
+                  if (tareas_agrupadas.Agrupados[j]["Cableado"]==posNPA){
+                      cantidad_oe=tareas_agrupadas.Agrupados[j]["Cantidad"]*cantidad
+                      descripcion=tareas_agrupadas.Agrupados[j]["Descripción"]
+                      for (let k = 0; k < posiciones.NPAyPosiciones.length; k++){
+                          if (posiciones.NPAyPosiciones[k]["Servicio"]==tareas_agrupadas.Agrupados[j]["Código"]){
+                              posicion=posiciones.NPAyPosiciones[k]["Posición"]
+                              precio=posiciones.NPAyPosiciones[k]["Precio"]
+                              precio_total=precio*cantidad_oe
+                              }
+                      }
+                      fila=[npa_proveedor, "0010", posicion, descripcion, fecha_entrega, cantidad_oe, precio, precio_total]
+                      filas.push(fila)}
+          }}   
+  }
+  let ordenador=[]
+  for (let i=0; i<filas.length;i++){
+    ordenador.push(filas[i][2])
+  }
+  ordenador.sort(function(a, b){return a - b})
+
+  for(var i = ordenador.length -1; i >=0; i--){
+    if(ordenador.indexOf(ordenador[i]) !== i) ordenador.splice(i,1);
+  }    
+
+  const titulo3=document.createElement("span")
+  titulo3.innerHTML= "Plantilla de OE generada"
+  titulo3.setAttribute("id","generado")
+  titulo3.setAttribute("class","h4")
+
+  const boton3 = document.createElement("button")
+  boton3.innerHTML = "Copiar"
+  boton3.setAttribute("id","boton3")
+  boton3.setAttribute("onclick", "copiar_tabla('myTable3')");
+  boton3.setAttribute("class", "btn btn-success");
+  boton3.setAttribute("title", "copia en el portapapel, para luego poder pegar en el excel");
+
+  const boton4 = document.createElement("button")
+  boton4.innerHTML = "Editar"
+  boton4.setAttribute("id","boton4")
+  boton4.setAttribute("onclick", "editar_tareas()");
+  boton4.setAttribute("class", "btn btn-success");
+
+  const boton5 = document.createElement("button")
+  boton5.innerHTML = "Plantilla WE"
+  boton5.setAttribute("id","boton5")
+  boton5.setAttribute("onclick", "generar_WE()");
+  boton5.setAttribute("class", "btn btn-primary");
+  boton5.setAttribute("title", "genera plantilla WE para pegar en Excel y completar cuando tengas nro de OE");
+
+  const titulo4=document.createElement("p")
+  titulo4.innerHTML= "Proveedor: "+ nro_proveedor+" ("+nombre_proveedor+")"
+  titulo4.setAttribute("id","proveedor")
+  titulo4.setAttribute("class","h5")
+
+//    let col3 = ["NPA","Posición","Línea","Descripción", "Fecha", "Cantidad","P. Unit","P. Total"];
+
+  const table3 = document.createElement("table");
+  table3.setAttribute("id", "myTable3");
+  table3.setAttribute("class", "table");
+/*     let tr3 = table3.insertRow(-1);                   // table row.
+
+  for (let i = 0; i < col3.length; i++) {
+      let th3 = document.createElement("th");      // table header.
+      th3.innerHTML = col3[i];
+      tr3.appendChild(th3);
+  }
 */
+  divShowData3.innerHTML = "";
+  divShowData3.appendChild(titulo3)
+  divShowData3.appendChild(boton3)
+  divShowData3.appendChild(boton4)
+
+  for (let i = 0; i < ordenador.length; i++){
+      cantidad_oe=0
+      tr = table3.insertRow(-1);
+      for (let j = 0; j < filas.length; j++){
+          if (ordenador[i]==filas[j][2]){
+            posicion=filas[j][2]
+            descripcion=filas[j][3]
+            precio=filas[j][6]
+            cantidad_oe=parseFloat(cantidad_oe)+parseFloat(filas[j][5])
+          }
+      }
+      tr.insertCell(-1).innerHTML = npa_proveedor;
+      tr.insertCell(-1).innerHTML = "0010";
+      tr.insertCell(-1).innerHTML = posicion;
+      tr.insertCell(-1).innerHTML = descripcion;
+      tr.insertCell(-1).innerHTML = fecha_entrega;
+      tr.insertCell(-1).innerHTML = cantidad_oe.toString().replace('.', ',');
+      tr.insertCell(-1).innerHTML = precio.toString().replace('.', ',');
+      tr.insertCell(-1).innerHTML = ((Math.round(precio*cantidad_oe*100))/100).toString().replace('.', ',')
+      total=(Math.round((total+(precio*cantidad_oe)))*100)/100
+  }
+  let titulo5 = document.createElement("span")
+  titulo5.innerHTML= "Monto total OE:  $" + total
+  titulo5.setAttribute("id","total_OE")
+  titulo5.setAttribute("class","h5")
+  divShowData3.appendChild(titulo4)
+  divShowData3.appendChild(titulo5);
+  divShowData3.appendChild(table3);
+  divShowData3.appendChild(boton5)
+}
